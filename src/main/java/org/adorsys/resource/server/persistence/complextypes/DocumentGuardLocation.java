f5d4e0803052bdf54639169c1163531609fbafa3
electronicpeter <git@electronicpeter.de>
2018-01-06 19:54:11 +0100

    alle StringMethoden aus den Location Klassen raus

--- src/main/java/org/adorsys/resource/server/persistence/complextypes/DocumentGuardLocation.java
+++ src/main/java/org/adorsys/resource/server/persistence/complextypes/DocumentGuardLocation.java
@@ -6,27 +6,15 @@ import org.adorsys.resource.server.basetypes.DocumentKeyID;
 /**
  * Created by peter on 23.12.2017 at 18:33:14.
  * 
- * @TODO: Add following fields:
- * - GuardKeyID ??
  */
 public class DocumentGuardLocation {
 	private static final long serialVersionUID = -3042461057378981231L;
 
-	
 	public static final String GUARD_NAME_COMPONENT_SEPARATOR = ".";
 	
 	private DocumentKeyID documentKeyID;
 	private KeyStoreLocation keyStoreLocation;
 
-	/*
-	public DocumentGuardLocation() {}
-
-
-	public DocumentGuardLocation(String value) {
-		super(value);
-		fromString(value);
-	}
-*/
 	public DocumentGuardLocation(KeyStoreLocation keyStoreLocation, DocumentKeyID documentKeyID) {
 		this.documentKeyID = documentKeyID;
 		this.keyStoreLocation = keyStoreLocation;
@@ -41,40 +29,10 @@ public class DocumentGuardLocation {
 	}
 
 	public ObjectHandle getLocationHandle(){
-		return new ObjectHandle(keyStoreLocation.getKeyStoreBucketName().getValue(), toFileName());
+		return new ObjectHandle(
+				keyStoreLocation.getKeyStoreBucketName().getValue(),
+				keyStoreLocation.toFileName() + GUARD_NAME_COMPONENT_SEPARATOR + documentKeyID.getValue());
 	}
-
-	public String toFileName() {
-		return toFileName(keyStoreLocation, documentKeyID);
-	}
-
-	public static String toFileName(KeyStoreLocation keyStoreLocation, DocumentKeyID documentKeyID) {
-		return keyStoreLocation.toFileName() + GUARD_NAME_COMPONENT_SEPARATOR + documentKeyID.getValue() ;
-	}
-
-	/*
-
-	private static String toString(KeyStoreLocation keyStoreLocation, DocumentKeyID documentKeyID){
-		return toFileName(keyStoreLocation, documentKeyID) + DocumentGuardBucketName.BUCKET_SEPARATOR + keyStoreLocation.getKeyStoreBucketName().getValue();
-	}
-
-
-	private void fromString(String guardFQN){
-		
-		String bucketNameStr = StringUtils.substringAfterLast(guardFQN, DocumentGuardBucketName.BUCKET_SEPARATOR);
-		
-		String documentGuardfileName = StringUtils.substringBeforeLast(guardFQN, DocumentGuardBucketName.BUCKET_SEPARATOR);
-		String documentKeyIDName = StringUtils.substringAfterLast(documentGuardfileName, GUARD_NAME_COMPONENT_SEPARATOR);
-		String keyStoreFileName = StringUtils.substringBeforeLast(documentGuardfileName, GUARD_NAME_COMPONENT_SEPARATOR);
-		
-		// TOdo Peter refactor
-		String keyStoreType = StringUtils.substringAfterLast(keyStoreFileName, KeyStoreLocation.FILE_EXTENSION_SEPARATOR);
-		String keyStoreId = StringUtils.substringBeforeLast(keyStoreFileName, KeyStoreLocation.FILE_EXTENSION_SEPARATOR);
-    	
-		keyStoreLocation = new KeyStoreLocation(new KeyStoreBucketName(bucketNameStr), new KeyStoreID(keyStoreId), new KeyStoreType(keyStoreType));
-		documentKeyID = new DocumentKeyID(documentKeyIDName);
-	}
-*/
 
 	@Override
 	public String toString() {
