90b7a3a3b672d47596e0fc4275686687b3be65a7
electronicpeter <git@electronicpeter.de>
2018-01-24 13:27:22 +0100

    dorest.sh requests als curl script

--- dorest.sh
+++ dorest.sh
@@ -4,5 +4,18 @@ curl -X PUT -H 'Content-Type: application/json' -H 'Accept: application/json' -i
 echo "get README.txt of home dir"
 curl -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: affe' -H 'password: rkp' -i 'http://localhost:8080/document/%22README.txt%22'
 
+echo "speichere ein weiteres dokument"
+curl -X PUT -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: affe' -H 'password: rkp' -i http://localhost:8080/document --data '{
+  "documentFQN": "deeper/and/deeper/README.txt",
+  "documentContent": {
+    "value": [
+    1,2,3,4,5,6,7,8
+    ]
+  }
+}'
+
+echo "get README.txt of home dir"
+curl -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: affe' -H 'password: rkp' -i 'http://localhost:8080/document/%22deeper/and/deeper/README.txt%22'
+
 echo "delete user"
 curl -X DELETE -H 'Content-Type: application/json' -H 'Accept: application/json' -i http://localhost:8080/internal/user --data '{"userID":"affe", "readKeyPassword":"rkp"}'
