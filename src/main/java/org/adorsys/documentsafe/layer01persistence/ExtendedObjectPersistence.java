a4761c1a6631ee5fd6e8c6ade66177e0e7cadf34
electronicpeter <git@electronicpeter.de>
2018-01-24 10:12:55 +0100

    JWE Type nun nicht mehr als Extension, sondern in der MetaInfo, KeyID nicht nur im JWE Header, sondern auch in der MetaInfo

--- src/main/java/org/adorsys/documentsafe/layer01persistence/ExtendedObjectPersistence.java
+++ src/main/java/org/adorsys/documentsafe/layer01persistence/ExtendedObjectPersistence.java
@@ -12,8 +12,10 @@ import org.adorsys.documentsafe.layer00common.exceptions.BaseExceptionHandler;
 import org.adorsys.documentsafe.layer01persistence.exceptions.ExtendedPersistenceException;
 import org.adorsys.documentsafe.layer01persistence.exceptions.FileExistsException;
 import org.adorsys.documentsafe.layer01persistence.keysource.KeySource;
+import org.adorsys.documentsafe.layer01persistence.types.EncryptionType;
 import org.adorsys.documentsafe.layer01persistence.types.KeyID;
 import org.adorsys.documentsafe.layer01persistence.types.OverwriteFlag;
+import org.adorsys.documentsafe.layer01persistence.types.PersistenceLayerContentMetaInfoUtil;
 import org.adorsys.documentsafe.layer01persistence.types.complextypes.BucketPath;
 import org.adorsys.encobject.domain.ContentMetaInfo;
 import org.adorsys.encobject.domain.ObjectHandle;
@@ -21,127 +23,128 @@ import org.adorsys.encobject.params.EncryptionParams;
 import org.adorsys.encobject.service.StoreConnection;
 import org.adorsys.jjwk.selector.JWEEncryptedSelector;
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.security.Key;
 
 /**
- * This class jwe encrypt and store bytes, loads and jwe decrypt bytes. For this purpose, this class always add a ".jwe" to the file name. 
- * It will stripp this".jwe" file extension before returning the byte to the caller. 
- * 
+ * This class jwe encrypt and store bytes, loads and jwe decrypt bytes. For this purpose, this class always add a ".jwe" to the file name.
+ * It will stripp this".jwe" file extension before returning the byte to the caller.
+ * <p>
  * e.g.:
  * -> if you store "peter.doc" in the storage you will find "peter.doc.jwe"
  * -> if you request "peter.doc", we will look for "peter.doc.jwe"
- * 
- * @author fpo
  *
+ * @author fpo
  */
 public class ExtendedObjectPersistence {
+    private final static Logger LOGGER = LoggerFactory.getLogger(ExtendedObjectPersistence.class);
 
-	private static final String EXTENSION = ".jwe";
+    private DefaultJWEDecrypterFactory decrypterFactory = new DefaultJWEDecrypterFactory();
 
-	private DefaultJWEDecrypterFactory decrypterFactory = new DefaultJWEDecrypterFactory();
+    private final StoreConnection blobStoreConnection;
 
-	private final StoreConnection blobStoreConnection;
+    public ExtendedObjectPersistence(StoreConnection blobStoreConnection) {
+        this.blobStoreConnection = blobStoreConnection;
+    }
 
-	public ExtendedObjectPersistence(StoreConnection blobStoreConnection) {
-		this.blobStoreConnection = blobStoreConnection;
-	}
-	
-	/**
-	 * Encrypt and stores an byte array given additional meta information and encryption details.
-	 * 
-	 * 
-	 * @param data : unencrypted version of bytes to store
-	 * @param metaInfo : document meta information. e.g. content type, compression, expiration
-	 * @param location : location of the document. Includes container name (bucket) and file name.
-	 * @param keySource : key producer. Return a key given the keyId
-	 * @param keyID : id of the key to be used from source to encrypt the docuement.
-	 * @param encParams
-	 */
-	public void storeObject(byte[] data, ContentMetaInfo metaInfo, ObjectHandle location, KeySource keySource, KeyID keyID,
-							EncryptionParams encParams, OverwriteFlag overwrite) {
-		
-		try {
+    /**
+     * Encrypt and stores an byte array given additional meta information and encryption details.
+     *
+     * @param data      : unencrypted version of bytes to store
+     * @param metaInfo  : document meta information. e.g. content type, compression, expiration
+     * @param location  : location of the document. Includes container name (bucket) and file name.
+     * @param keySource : key producer. Return a key given the keyId
+     * @param keyID     : id of the key to be used from source to encrypt the docuement.
+     * @param encParams
+     */
+    public void storeObject(byte[] data, ContentMetaInfo metaInfo, ObjectHandle location, KeySource keySource, KeyID keyID,
+                            EncryptionParams encParams, OverwriteFlag overwrite) {
 
-			// We accept empty meta info
-			if (metaInfo == null)metaInfo=new ContentMetaInfo();
-	
-			// Retrieve the key.
-			Key key = keySource.readKey(keyID);
-	
-			// Encryption params is optional. If not provided, we select an
-			// encryption param based on the key selected.
-			if (encParams == null) encParams = ExtendedEncParamSelector.selectEncryptionParams(key);
+        try {
 
-			Builder headerBuilder = new JWEHeader.Builder(encParams.getEncAlgo(), encParams.getEncMethod()).keyID(keyID.getValue());
-			ContentMetaInfoUtils.metaInfo2Header(metaInfo, headerBuilder);
-	
-			JWEHeader header = headerBuilder.build();
-	
-			JWEEncrypter jweEncrypter = JWEEncryptedSelector.geEncrypter(key, encParams.getEncAlgo(),
-					encParams.getEncMethod());
-	
-			JWEObject jweObject = new JWEObject(header, new Payload(data));
-			jweObject.encrypt(jweEncrypter);
-	
-			String jweEncryptedObject = jweObject.serialize();
-	
-			byte[] bytesToStore = jweEncryptedObject.getBytes("UTF-8");
+            // We accept empty meta info
+            if (metaInfo == null) metaInfo = new ContentMetaInfo();
 
-			ObjectHandle extendedLocation = addExtension(location);
-			if (overwrite == OverwriteFlag.FALSE) {
-				if (blobStoreConnection instanceof ExtendedBlobStoreConnection) {
-					ExtendedBlobStoreConnection extendedBlobStoreConnection = (ExtendedBlobStoreConnection) blobStoreConnection;
-					BucketPath bp = new BucketPath(location.getContainer());
-					String filename = location.getName();
+            // Retrieve the key.
+            Key key = keySource.readKey(keyID);
 
-					boolean blobExists = extendedBlobStoreConnection.blobExists(extendedLocation);
-					if (blobExists) {
-						throw new FileExistsException("File " + extendedLocation.getContainer() + " " + extendedLocation.getName() + " already exists");
-					}
-				} else {
-					throw new BaseException("dont know how to check for existing file");
-				}
-			}
-			blobStoreConnection.putBlob(extendedLocation, bytesToStore);
-		} catch (Exception e){
-			BaseExceptionHandler.handle(e);
-		}
-	}
+            PersistenceLayerContentMetaInfoUtil.setKeyID(metaInfo, keyID);
+            PersistenceLayerContentMetaInfoUtil.setEncryptionType(metaInfo, EncryptionType.JWE);
 
-	private ObjectHandle addExtension(ObjectHandle location) {
-		return new ObjectHandle(location.getContainer(), location.getName()+ EXTENSION);
-	}
+            // Encryption params is optional. If not provided, we select an
+            // encryption param based on the key selected.
+            if (encParams == null) encParams = ExtendedEncParamSelector.selectEncryptionParams(key);
 
-	public PersistentObjectWrapper loadObject(ObjectHandle location, KeySource keySource) {
-		
-		try {
-			if (location == null)
-				throw new ExtendedPersistenceException("Location for Object must not be null.");
-			
-			location = addExtension(location);
-	
-			byte[] jweEncryptedBytes = blobStoreConnection.getBlob(location);
-			String jweEncryptedObject = IOUtils.toString(jweEncryptedBytes, "UTF-8");
+            Builder headerBuilder = new JWEHeader.Builder(encParams.getEncAlgo(), encParams.getEncMethod()).keyID(keyID.getValue());
+            ContentMetaInfoUtils.metaInfo2Header(metaInfo, headerBuilder);
 
-			JWEObject jweObject = JWEObject.parse(jweEncryptedObject);
+            JWEHeader header = headerBuilder.build();
 
-			KeyID keyID = new KeyID(jweObject.getHeader().getKeyID());
-			Key key = keySource.readKey(keyID);
+            JWEEncrypter jweEncrypter = JWEEncryptedSelector.geEncrypter(key, encParams.getEncAlgo(),
+                    encParams.getEncMethod());
 
-			if (key == null) {
-				throw new ExtendedPersistenceException("can not read key with keyID " + keyID + " from keySource of class " + keySource.getClass().getName());
-			}
+            JWEObject jweObject = new JWEObject(header, new Payload(data));
+            jweObject.encrypt(jweEncrypter);
 
-			JWEDecrypter decrypter = decrypterFactory.createJWEDecrypter(jweObject.getHeader(), key);
-			jweObject.decrypt(decrypter);
+            String jweEncryptedObject = jweObject.serialize();
 
-			ContentMetaInfo metaInfo = new ContentMetaInfo();
-			ContentMetaInfoUtils.header2MetaInfo(jweObject.getHeader(), metaInfo);
-			return new PersistentObjectWrapper(jweObject.getPayload().toBytes(), metaInfo, keyID, location);
-		} catch (Exception e) {
-			throw BaseExceptionHandler.handle(e);
-		}
+            byte[] bytesToStore = jweEncryptedObject.getBytes("UTF-8");
 
-	}
+            if (overwrite == OverwriteFlag.FALSE) {
+                if (blobStoreConnection instanceof ExtendedBlobStoreConnection) {
+                    ExtendedBlobStoreConnection extendedBlobStoreConnection = (ExtendedBlobStoreConnection) blobStoreConnection;
+                    BucketPath bp = new BucketPath(location.getContainer());
+                    String filename = location.getName();
+
+                    boolean blobExists = extendedBlobStoreConnection.blobExists(location);
+                    if (blobExists) {
+                        throw new FileExistsException("File " + location.getContainer() + " " + location.getName() + " already exists");
+                    }
+                } else {
+                    throw new BaseException("dont know how to check for existing file");
+                }
+            }
+            blobStoreConnection.putBlob(location, bytesToStore);
+        } catch (Exception e) {
+            BaseExceptionHandler.handle(e);
+        }
+    }
+
+    public PersistentObjectWrapper loadObject(ObjectHandle location, KeySource keySource) {
+
+        try {
+            if (location == null)
+                throw new ExtendedPersistenceException("Location for Object must not be null.");
+
+            byte[] jweEncryptedBytes = blobStoreConnection.getBlob(location);
+            String jweEncryptedObject = IOUtils.toString(jweEncryptedBytes, "UTF-8");
+
+            JWEObject jweObject = JWEObject.parse(jweEncryptedObject);
+            ContentMetaInfo metaInfo = new ContentMetaInfo();
+            ContentMetaInfoUtils.header2MetaInfo(jweObject.getHeader(), metaInfo);
+            EncryptionType encryptionType = PersistenceLayerContentMetaInfoUtil.getEncryptionnType(metaInfo);
+            if (! encryptionType.equals(EncryptionType.JWE)) {
+                throw new BaseException("Expected EncryptionType is " + EncryptionType.JWE + " but was " + encryptionType);
+            }
+            KeyID keyID = PersistenceLayerContentMetaInfoUtil.getKeyID(metaInfo);
+            KeyID keyID2 = new KeyID(jweObject.getHeader().getKeyID());
+            if (!keyID.equals(keyID2)) {
+                throw new BaseException("die in der MetaInfo hinterlegte keyID " + keyID + " passt nicht zu der im header hinterlegten KeyID " + keyID2);
+            }
+            Key key = keySource.readKey(keyID);
+
+            if (key == null) {
+                throw new ExtendedPersistenceException("can not read key with keyID " + keyID + " from keySource of class " + keySource.getClass().getName());
+            }
+
+            JWEDecrypter decrypter = decrypterFactory.createJWEDecrypter(jweObject.getHeader(), key);
+            jweObject.decrypt(decrypter);
+            return new PersistentObjectWrapper(jweObject.getPayload().toBytes(), metaInfo, keyID, location);
+        } catch (Exception e) {
+            throw BaseExceptionHandler.handle(e);
+        }
+
+    }
 }
---
+++ src/main/java/org/adorsys/documentsafe/layer01persistence/types/EncryptionType.java
@@ -0,0 +1,9 @@
+package org.adorsys.documentsafe.layer01persistence.types;
+
+/**
+ * Created by peter on 24.01.18 at 10:01.
+ */
+public enum EncryptionType {
+    NONE,
+    JWE
+}
