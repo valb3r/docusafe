33d0b2f8d0990061c91d1841159935df7a6fcce9
pspex <pspex@adorsys.de>
2018-01-26 10:30:45 +0100

    curltests mit sleep, um ergebnis abgreifen zu koennen

--- REST-Curl.sh
+++
@@ -1,7 +0,0 @@
-echo "json ------"
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:8080/info
-echo ""
-
-echo "json ------"
-curl -X PUT -H "Accept: application/json" -H "Content-Type: application/json" -d '{"info": "affe","documentKeyID": "123","documentLocation": {"documentID": "id","documentBucketPath": "bucket/1/2/3"}' http://localhost:8080/put
-echo ""
