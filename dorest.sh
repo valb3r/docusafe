c2fccb2e8961cd145924ecc3608248a5ca0a0d06
pspex <pspex@adorsys.de>
2018-01-30 13:54:48 +0100

    dorest.sh test verschärft

--- dorest.sh
+++ dorest.sh
@@ -2,20 +2,53 @@
 trap error ERR
 
 function error () {
-	echo "an error occured"
+	print "an error occured"
 	exit 1
 }
 
-echo "delete user, if exists, ignore error"
-curl -X DELETE -H 'Content-Type: application/json' -H 'Accept: application/json' -i http://localhost:8080/internal/user --data '{"userID":"peter", "readKeyPassword":"rkp"}' > curl1.log
+function print () {
+	{
+	echo "==================================================================================="
+	echo $1
+	echo " "
+	} | tee -a curl.log
+}
 
-echo "create user"
-curl -f -X PUT -H 'Content-Type: application/json' -H 'Accept: application/json' -i http://localhost:8080/internal/user --data '{"userID":"peter", "readKeyPassword":"rkp"}' > curl2.log
+function checkGuards() {
+	user=$1
+	expected=$2
 
-echo "get README.txt of home dir"
-curl -f -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: peter' -H 'password: rkp' -i 'http://localhost:8080/document/%22README.txt%22' > curl3.log
+	guardKeys=$(find target/filesystemstorage -type f |  grep "^target/filesystemstorage/BP-$user/.KEYSTORE" | grep bucketGuardKey | wc -l)
+	if (( guardKeys == expected )) 
+	then
+		echo "ok Anzahl von $user GuardKeys ist $expected.  Das ist fein." | tee -a curl.log
+	else
+		print "DANGER DANGER ACHTUNG FEHLER. ANZAHL DER GUARD KEYs von $user IST NICHT KORREKT expected $expected but was $guardKeys"
+		exit 1;
+	fi
 
-echo "save deep document"
+	guards=$(find target/filesystemstorage -type f |  grep "^target/filesystemstorage/BP-$user/.KEYSTORE/.*UBER." | wc -l)
+	if (( guards == expected )) 
+	then
+		echo "ok Anzahl von $user Guards ist $expected.  Das ist fein." | tee -a curl.log
+	else
+		print "DANGER DANGER ACHTUNG FEHLER. ANZAHL DER GUARD von $user IST NICHT KORREKT expected $expected but was $guards"
+		exit 1;
+	fi
+}
+
+rm -f curl.log
+print "delete user, if exists, ignore error"
+curl -X DELETE -H 'Content-Type: application/json' -H 'Accept: application/json' -i http://localhost:8080/internal/user --data '{"userID":"peter", "readKeyPassword":"rkp"}' >> curl.log
+curl -X DELETE -H 'Content-Type: application/json' -H 'Accept: application/json' -i http://localhost:8080/internal/user --data '{"userID":"francis", "readKeyPassword":"passWordXyZ"}' >> curl.log
+
+print "create user"
+curl -f -X PUT -H 'Content-Type: application/json' -H 'Accept: application/json' -i http://localhost:8080/internal/user --data '{"userID":"peter", "readKeyPassword":"rkp"}' >> curl.log
+
+print "get README.txt of home dir"
+curl -f -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: peter' -H 'password: rkp' -i 'http://localhost:8080/document/%22README.txt%22' >> curl.log
+
+print "save deep document"
 curl -f -X PUT -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: peter' -H 'password: rkp' -i http://localhost:8080/document --data '{
   "documentFQN": "deeper/and/deeper/README.txt",
   "documentContent": {
@@ -23,46 +56,41 @@ curl -f -X PUT -H 'Content-Type: application/json' -H 'Accept: application/json'
     1,2,3,4,5,6,7,8
     ]
   }
-}' > curl4.log
+}' >> curl.log
 
-echo "get deep document"
-curl -f -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: peter' -H 'password: rkp' -i 'http://localhost:8080/document/%22deeper/and/deeper/README.txt%22' > curl5.log
+print "get deep document"
+curl -f -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: peter' -H 'password: rkp' -i 'http://localhost:8080/document/%22deeper/and/deeper/README.txt%22' >> curl.log
 
-echo "link deep document"
+print "link deep document"
 curl -f -X PUT -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: peter' -H 'password: rkp' -i http://localhost:8080/document/link --data '{
   "source": "deeper/and/deeper/README.txt",
   "destination": "green/bucket/README.txt"
-}' > curl6.log
+}' >> curl.log
 
-echo "get linked document"
-curl -f -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: peter' -H 'password: rkp' -i 'http://localhost:8080/document/%22green/bucket/README.txt%22' > curl7.log
+print "get linked document"
+curl -f -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: peter' -H 'password: rkp' -i 'http://localhost:8080/document/%22green/bucket/README.txt%22' >> curl.log
 
-echo "--------------------- check filesystem ----------"
-find target/filesystemstorage
-echo "----------------------"
-guards=$(find target/filesystemstorage -name '*bucketGuardKey' | wc -l)
-if (( guards == 3 )) 
-then
-	echo "ok Anzahl der Guards ist 3.  Das ist fein."
-else
-	echo "DANGER DANGER ACHTUNG FEHLER. ANZAHL DER GUARDS IST NICHT KORREKT"
-	exit 1;
-fi
+print "create user francis"
+curl -X PUT -H 'Content-Type: application/json' -H 'Accept: application/json' -i http://localhost:8080/internal/user --data '{"userID":"francis", "readKeyPassword":"passWordXyZ"}' >> curl.log
 
-echo "EVERYTHING WENT FINE so FAR"
-
-echo "create user francis"
-curl -X PUT -H 'Content-Type: application/json' -H 'Accept: application/json' -i http://localhost:8080/internal/user --data '{"userID":"francis", "readKeyPassword":"passWordXyZ"}' > curl8.log
-
-echo "grant peters deeper/and/deeper to francis"
+print "grant peters deeper/and/deeper to francis"
 curl -X PUT -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: peter' -H 'password: rkp' -i http://localhost:8080/grant/document --data '{
   "documentDirectoryFQN": "deeper/and/deeper",
   "receivingUser": "francis",
   "accessType" : "WRITE"
-}' > curl9.log
+}' >> curl.log
 
-echo "francis liest Document von Peter"
-curl -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: francis' -H 'password: passWordXyZ' -i 'http://localhost:8080/grant/document/peter/%22/deeper/and/deeper/README.txt%22' > curl10.log
+print "francis liest Document von Peter"
+curl -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'userid: francis' -H 'password: passWordXyZ' -i 'http://localhost:8080/grant/document/peter/%22/deeper/and/deeper/README.txt%22' >> curl.log
 
-echo "delete user"
-curl -f -X DELETE -H 'Content-Type: application/json' -H 'Accept: application/json' -i http://localhost:8080/internal/user --data '{"userID":"peter", "readKeyPassword":"rkp"}' > curl.100.log
+print "check filesystem"
+find target/filesystemstorage -type f >> curl.log
+
+checkGuards peter   3
+checkGuards francis 2
+
+print "EVERYTHING WENT FINE so FAR"
+
+print "delete user"
+curl -f -X DELETE -H 'Content-Type: application/json' -H 'Accept: application/json' -i http://localhost:8080/internal/user --data '{"userID":"peter", "readKeyPassword":"rkp"}' >> curl.log
+curl -f -X DELETE -H 'Content-Type: application/json' -H 'Accept: application/json' -i http://localhost:8080/internal/user --data '{"userID":"francis", "readKeyPassword":"passWordXyZ"}' >> curl.log
