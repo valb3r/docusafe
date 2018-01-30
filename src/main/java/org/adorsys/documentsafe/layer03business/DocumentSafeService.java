890978d83fcc3053f76bbc0e0ab83eb029d638d5
pspex <pspex@adorsys.de>
2018-01-30 18:25:47 +0100

    interface nur declarativ umgestellt

--- src/main/java/org/adorsys/documentsafe/layer03business/DocumentSafeService.java
+++ src/main/java/org/adorsys/documentsafe/layer03business/DocumentSafeService.java
@@ -1,11 +1,10 @@
 package org.adorsys.documentsafe.layer03business;
 
-import org.adorsys.documentsafe.layer02service.types.complextypes.DocumentDirectory;
 import org.adorsys.documentsafe.layer03business.types.AccessType;
 import org.adorsys.documentsafe.layer03business.types.UserID;
+import org.adorsys.documentsafe.layer03business.types.complex.DSDocument;
 import org.adorsys.documentsafe.layer03business.types.complex.DocumentDirectoryFQN;
 import org.adorsys.documentsafe.layer03business.types.complex.DocumentFQN;
-import org.adorsys.documentsafe.layer03business.types.complex.DSDocument;
 import org.adorsys.documentsafe.layer03business.types.complex.UserIDAuth;
 
 /**
@@ -13,10 +12,14 @@ import org.adorsys.documentsafe.layer03business.types.complex.UserIDAuth;
  */
 public interface DocumentSafeService {
     void createUser(UserIDAuth userIDAuth);
-    void storeDocument(UserIDAuth userIDAuth, DSDocument dsDocument);
     void destroyUser(UserIDAuth userIDAuth);
+
+    void storeDocument(UserIDAuth userIDAuth, DSDocument dsDocument);
     DSDocument readDocument(UserIDAuth userIDAuth, DocumentFQN documentFQN);
-    DSDocument readDocument(UserIDAuth userIDAuth, UserID documentOwner, DocumentFQN documentFQN);
-    void linkDocument(UserIDAuth userIDAuth, DocumentFQN sourceDocumentFQN, DocumentFQN destinationDocumentFQN);
+
     void grantAccessToUserForFolder(UserIDAuth userIDAuth, UserID receiverUserID, DocumentDirectoryFQN documentDirectoryFQN, AccessType accessType);
+    DSDocument readDocument(UserIDAuth userIDAuth, UserID documentOwner, DocumentFQN documentFQN);
+
+    void linkDocument(UserIDAuth userIDAuth, DocumentFQN sourceDocumentFQN, DocumentFQN destinationDocumentFQN);
+
 }
