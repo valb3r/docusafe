036e6ef4514f65b848d405dfb191263285ea7609
pspex <pspex@adorsys.de>
2018-04-26 18:01:10 +0200

    DOC-23 removed deprecated api for swagger

--- docusafe-rest/src/main/java/org/adorsys/docusafe/rest/WebMvcConfig.java
+++ docusafe-rest/src/main/java/org/adorsys/docusafe/rest/WebMvcConfig.java
@@ -2,11 +2,6 @@ package org.adorsys.docusafe.rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
-import org.adorsys.docusafe.rest.adapter.SpringfoxJsonToGsonAdapter;
-import org.adorsys.docusafe.service.types.AccessType;
-import org.adorsys.docusafe.service.types.DocumentContent;
-import org.adorsys.docusafe.service.types.DocumentKeyID;
 import org.adorsys.docusafe.business.types.UserID;
 import org.adorsys.docusafe.business.types.complex.DocumentDirectoryFQN;
 import org.adorsys.docusafe.business.types.complex.DocumentFQN;
@@ -17,30 +12,24 @@ import org.adorsys.docusafe.rest.adapter.DocumentDirectoryFQNJsonAdapter;
 import org.adorsys.docusafe.rest.adapter.DocumentFQNJsonAdapter;
 import org.adorsys.docusafe.rest.adapter.DocumentKeyIDJsonAdapter;
 import org.adorsys.docusafe.rest.adapter.ReadKeyPasswordJsonAdapter;
+import org.adorsys.docusafe.rest.adapter.SpringfoxJsonToGsonAdapter;
 import org.adorsys.docusafe.rest.adapter.UserIDJsonAdapter;
+import org.adorsys.docusafe.service.types.AccessType;
+import org.adorsys.docusafe.service.types.DocumentContent;
+import org.adorsys.docusafe.service.types.DocumentKeyID;
 import org.adorsys.encobject.domain.ReadKeyPassword;
 import org.adorsys.encobject.types.BucketName;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiListing;
-import springfox.documentation.service.ResourceListing;
-import springfox.documentation.spi.DocumentationType;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.spring.web.json.Json;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.SecurityConfiguration;
-import springfox.documentation.swagger.web.SwaggerResource;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
@@ -50,12 +39,13 @@ import java.util.List;
 @SpringBootApplication(exclude = {JacksonAutoConfiguration.class, MongoAutoConfiguration.class})
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(createGsonHttpMessageConverter());
-        super.configureMessageConverters(converters);
+        RestTemplate r = new RestTemplate();
+        r.setMessageConverters(converters);
     }
 
     private GsonHttpMessageConverter createGsonHttpMessageConverter() {
