784dd73c3597ba05dce2ca4b1742e64602437ac0
pspex <git@electronicpeter.de>
2018-06-15 18:20:20 +0200

    HowTo Test etwas mehr kommentiert

--- docusafe-transactional/src/test/java/org/adorsys/docusafe/transactional/HowToUseTransactionalFileStoragePrototypeTest.java
+++ docusafe-transactional/src/test/java/org/adorsys/docusafe/transactional/HowToUseTransactionalFileStoragePrototypeTest.java
@@ -1,5 +1,6 @@
 package org.adorsys.docusafe.transactional;
 
+import org.adorsys.docusafe.business.exceptions.GuardException;
 import org.adorsys.docusafe.business.types.complex.BucketContentFQN;
 import org.adorsys.docusafe.business.types.complex.DSDocument;
 import org.adorsys.docusafe.business.types.complex.DSDocumentMetaInfo;
@@ -8,20 +9,23 @@ import org.adorsys.docusafe.business.types.complex.DocumentFQN;
 import org.adorsys.docusafe.service.types.DocumentContent;
 import org.adorsys.docusafe.transactional.types.TxID;
 import org.adorsys.encobject.types.ListRecursiveFlag;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Created by peter on 14.06.18 at 23:33.
- *
+ * <p>
  * Dieser Test zeigt das Zusammenspiel vom Systembenutzer, der Dateien ausserhalb einer Transaktion
  * für einen Benutzer in einem "INBOX" Folder ablegt.
  * Dann greift der Benutzer innerhalb einer Transaktion auf diese Datei zu.
  *
+ * Wichtig ist, dass der documentSafeService nicht benutzt wird, denn das TransactionalFileStorage Layer
+ * setzt auf dem documentSafeService auf.
  */
 // @SuppressWarnings("Duplicates")
-public class HowToUseTransactionalFileStoragePrototypeTest extends TransactionFileStorageBaseTest{
+public class HowToUseTransactionalFileStoragePrototypeTest extends TransactionFileStorageBaseTest {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(HowToUseTransactionalFileStoragePrototypeTest.class);
 
@@ -40,12 +44,32 @@ public class HowToUseTransactionalFileStoragePrototypeTest extends TransactionFi
 
         // prüfen, ob neue Dateien in der Inbox sind
         // da noch nichts angelegt wurde, passiert hier erst einmal nichts
-        checkForNewInFiles();
+        int filesMoved = checkForNewInFiles();
+        Assert.assertEquals(0, filesMoved);
 
         // stelle nun als system user eine Datei bereit
         transactionalFileStorage.storeDocument(systemUserIDAuth, userIDAuth.getUserID(), newDocument("file1"));
 
-        checkForNewInFiles();
+        // nun gibt es eine Datei, die wird innerhalb der Unterfunktion kopiert und dann gelöscht
+        filesMoved = checkForNewInFiles();
+        Assert.assertEquals(1, filesMoved);
+
+        // jetzt liegt noch nichts neues an
+        filesMoved = checkForNewInFiles();
+        Assert.assertEquals(0, filesMoved);
+
+        // Das Anlegen einer Datei in einem Unterverzeichnis der Inbox ist nicht gestattet, da der Grant immer nur
+        // Verzeichnisweise gilt, nicht für Unterverzeichnisse
+        boolean gotException = false;
+        try {
+            transactionalFileStorage.storeDocument(systemUserIDAuth, userIDAuth.getUserID(), newDocument("subdir/file1"));
+        } catch (GuardException e) {
+            gotException = true;
+        }
+        Assert.assertTrue(gotException);
+
+        // Wenn gewünscht, kann das granten hier erweitert werden. Dann könnte man verschiedenen
+        // Benutzern die Zugriff auf verschiedene Unterverzeichnisse gewähren.
 
     }
 
@@ -53,7 +77,8 @@ public class HowToUseTransactionalFileStoragePrototypeTest extends TransactionFi
         return new DSDocument(new DocumentFQN(filename), new DocumentContent("some content".getBytes()), new DSDocumentMetaInfo());
     }
 
-    private void checkForNewInFiles() {
+    // returns the number of files that have been imported
+    private int checkForNewInFiles() {
         LOGGER.info("check for new files in non transactional folder");
         // listDocuments (ohne txId) ist transaktionslos und bezieht sich damit immer
         // auf die inbox. Wenn man nur bestimmte Documente sehen möchte, dann man
@@ -63,7 +88,7 @@ public class HowToUseTransactionalFileStoragePrototypeTest extends TransactionFi
         if (list.getFiles().isEmpty()) {
             LOGGER.info("no new files found");
             // Nichts zu tuen, also return
-            return;
+            return 0;
         }
 
         // Es gibt also Dateien im der Inbox
@@ -98,5 +123,7 @@ public class HowToUseTransactionalFileStoragePrototypeTest extends TransactionFi
         list.getFiles().forEach(documentFQN -> {
             transactionalFileStorage.deleteDocument(userIDAuth, documentFQN);
         });
+
+        return list.getFiles().size();
     }
 }
