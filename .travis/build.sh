3baf921afb6b305b0db67a20d6f560b7c75bfa45
pspex <pspex@adorsys.de>
2018-10-31 11:19:57 +0100

    DOC-54 install anweisung von yml in build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-mvn --settings .travis/settings.xml clean verify -B -V
+# mvn --settings .travis/settings.xml clean verify -B -V
+
+mvn --settings .travis/settings.xml clean install -Dmaven.javadoc.skip=true -B -V
+mvn clean install -DSC-AMAZONS3=http://minio-psp-docusafe-performancetest.cloud.adorsys.de,simpleAccessKey,simpleSecretKey,us-east-1,travis.docusafe.${TRAVIS_BRANCH}
