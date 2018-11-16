421c7ebaa68e9129a962931cc78dbc4511dcde81
pspex <pspex@adorsys.de>
2018-11-16 12:36:38 +0100

    all beans in one class, cached still disabled but working

---
+++ docusafe-spring/src/main/java/org/adorsys/docusafe/spring/config/DocusafeSpringBeans.java
@@ -0,0 +1,51 @@
+package org.adorsys.docusafe.spring.config;
+
+import org.adorsys.docusafe.spring.factory.SpringExtendedStoreConnectionFactory;
+import org.adorsys.encobject.service.api.ExtendedStoreConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by peter on 02.10.18.
+ */
+@Configuration
+public class DocusafeSpringBeans {
+    private final static Logger LOGGER = LoggerFactory.getLogger(DocusafeSpringBeans.class);
+
+    public DocusafeSpringBeans() {
+        LOGGER.info("INIT");
+    }
+
+    @Bean
+    public ExtendedStoreConnection extendedStoreConnection(SpringExtendedStoreConnectionFactory factory) {
+        LOGGER.info(ExtendedStoreConnection.class.getName() + " is required as @Bean");
+        return factory.getExtendedStoreConnectionWithSubDir(null);
+    }
+
+    @Bean
+    public SpringExtendedStoreConnectionFactory factory(SpringDocusafeStoreconnectionProperties properties) {
+        LOGGER.info(SpringExtendedStoreConnectionFactory.class.getName() + " is required as @Bean");
+        return new SpringExtendedStoreConnectionFactory(properties);
+    }
+
+    /*
+    @Bean
+    SpringCachedTransactionalDocusafeServiceFactory factory(SpringExtendedStoreConnectionFactory connectionFactory,
+                                                            @Value("${docusafe.cache:true}") Boolean withCache) {
+        LOGGER.info(SpringCachedTransactionalDocusafeServiceFactory.class.getName() + " is required as @Bean");
+        return new SpringCachedTransactionalDocusafeServiceFactory(connectionFactory, withCache);
+    }
+
+    @Bean
+    public CachedTransactionalDocumentSafeService docusafeCachedTransactionalService(
+            SpringCachedTransactionalDocusafeServiceFactory springCachedTransactionalDocusafeServiceFactory,
+            @Value("${docusafe.cache:true}") Boolean withCache
+    ) {
+        LOGGER.info(CachedTransactionalDocumentSafeService.class.getName() + " is required as @Bean");
+        return springCachedTransactionalDocusafeServiceFactory.getCachedTransactionalDocumentSafeServiceWithSubdir(null);
+        return null;
+    }
+*/
+}
