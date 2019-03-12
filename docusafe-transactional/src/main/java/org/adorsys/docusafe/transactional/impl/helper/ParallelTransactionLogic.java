d1f0e3e6ca87d974c15028426c2d6236131493a3
pspex <pspex@adorsys.de>
2019-03-12 10:52:51 +0100

    DOC-78 prepare parallel commit

--- docusafe-transactional/src/main/java/org/adorsys/docusafe/transactional/impl/helper/ParallelTransactionLogic.java
+++ docusafe-transactional/src/main/java/org/adorsys/docusafe/transactional/impl/helper/ParallelTransactionLogic.java
@@ -1,9 +1,10 @@
 package org.adorsys.docusafe.transactional.impl.helper;
 
+import org.adorsys.docusafe.transactional.impl.TxIDHashMap;
 import org.adorsys.docusafe.transactional.impl.TxIDHashMapWrapper;
 
 public class ParallelTransactionLogic {
-    TxIDHashMapWrapper join(TxIDHashMapWrapper stateLastCommittedTx, TxIDHashMapWrapper stateAtBeginOfCurrentTex, TxIDHashMapWrapper stateAtEndOfCurrentTx) {
+    TxIDHashMapWrapper join(TxIDHashMapWrapper stateLastCommittedTx, TxIDHashMapWrapper stateAtBeginOfCurrentTex, TxIDHashMapWrapper stateAtEndOfCurrentTx, TxIDHashMap documentsReadInTx) {
         return null;
     }
 
